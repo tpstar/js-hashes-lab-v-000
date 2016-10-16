987cba793fd04e5f80ef3b43012bb97c556440c8
tpstar <hanlee12@gmail.com>
2016-10-16 00:18:04 -0500

    done

--- recipe.js
+++ recipe.js
@@ -4,6 +4,20 @@
 function addIngredient(object, ingredient, amount) {
   object[ingredient] = amount;
   return object;
-  
 }
 
+function removeIngredient(object, ingredient, amount) {
+  delete object[ingredient];
+  return object;
+}
+
+function updateIngredient (object, ingredient, amount) {
+  object[ingredient] = amount;
+  return object;
+}
+
+function readRecipe (object) {
+  for (var key in object) {
+    console.log("this recipe calls for " + object[key] + " of " + key);
+  }
+}
